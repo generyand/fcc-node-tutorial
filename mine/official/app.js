da92466b2a58644e254fb4936900e7ee8ab84a8e
generyand <generyan.dep@gmail.com>
2024-06-03 21:46:33 +0800

    paused at 4:02:47

--- mine/official/app.js
+++ mine/official/app.js
@@ -1,40 +1,11 @@
-const { readFile, writeFile } = require("fs").promises;
-// const util = require("util");
-// const readFilePromise = util.promisify(readFile);
-// const writeFilePromise = util.promisify(writeFile);
+const { createReadStream } = require("fs");
 
-const start = async () => {
-  try {
-    const first = await readFile("./content/first.txt", "utf8");
-    const second = await readFile("./content/second.txt", "utf8");
-    await writeFile(
-      "./content/result-mind-grenade.txt",
-      `WADUHECKKK\nFirst: ${first}\nSecond: ${second}`
-    );
-    console.log(first, second);
-  } catch (error) {
-    console.log(error);
-  }
-};
+const stream = createReadStream('./content/big.txt', {highWaterMark: 90000, encoding: 'utf8'});
 
-start();
+stream.on('data', (result) => {
+  console.log(result);
+})
 
-/* const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-}; */
-
-/* getText("./content/first.txt")
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  }); */
+stream.on('error', (err) => {
+  console.log(err);
+})
